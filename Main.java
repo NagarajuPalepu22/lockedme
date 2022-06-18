4788ba4c14cdac12ecd75784841e97f209051c76
NagarajuPalepu22 <107348585+NagarajuPalepu22@users.noreply.github.com>
2022-06-18 17:17:03 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+package com.lockedMe.Project;
+
+
+public class Main {
+
+	public static final String path = "C:\\Users\\DELL\\Desktop\\lockedme project\\com.lockedMe.project\\src\\com\\lockedMe";
+	public static void main(String[] args) {
+
+Menus menu = new Menus();
+menu.introScreen();
+menu.mainMenu();
+	}
+
+}
